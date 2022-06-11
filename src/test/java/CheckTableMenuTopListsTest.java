451d8fcfdf158c5aa1d450a8c4ef0e6a4e8ccc54
ServachakM <servachak.m.u@gmail.com>
2022-06-12 00:01:33 +0300

    Check table name test added

---
+++ src/test/java/CheckTableMenuTopListsTest.java
@@ -0,0 +1,23 @@
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import runner.BaseTest;
+
+public class CheckTableMenuTopListsTest extends BaseTest {
+
+    @Test
+    public void testTableNameMenuTopListsSubmenuTopRatedReal(){
+        final String URL = "https://www.99-bottles-of-beer.net/";
+
+        String expectedResult = "Top Rated Real Languages";
+
+        getDriver().get(URL);
+        getDriver().findElement(By.linkText("Top Lists")).click();
+        getDriver().findElement(By.linkText("Top Rated Real")).click();
+
+        String actualResult = getDriver().findElement(
+                By.xpath("//div[@id='main']/h2")).getText();
+
+        Assert.assertEquals(actualResult,expectedResult);
+    }
+}
