ff4454f4324558520a5ee9b6071a5540a9e8c84e
ArinaJur <66705803+ArinaJur@users.noreply.github.com>
2022-06-12 11:53:20 -0400

    AT_100.001 | [Structure][MainPage][StartPage][LyricsPage] | Create packages and pages MainPage, StartPage, LyricsPage (#9)

--- src/test/java/tests/CheckTableMenuTopListsTest.java
+++ src/test/java/tests/CheckTableMenuTopListsTest.java
@@ -1,3 +1,5 @@
+package tests;
+
 import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.Test;
--- src/test/java/tests/ShurikGTest.java
+++ src/test/java/tests/ShurikGTest.java
@@ -1,3 +1,5 @@
+package tests;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
