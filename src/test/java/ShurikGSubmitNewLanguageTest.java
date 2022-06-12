6ed176d4ad3448c211b8419b3cd3bddc96386ebc
Aleksandr G <99336539+ShurikGr@users.noreply.github.com>
2022-06-12 09:48:29 +0300

    added new test testErrorMessageSubmitLanguageButton (#4)

---
+++ src/test/java/ShurikGSubmitNewLanguageTest.java
@@ -0,0 +1,21 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import runner.BaseTest;
+
+public class ShurikGSubmitNewLanguageTest extends BaseTest {
+
+    @Test(description = "TC_700.001" )
+    public void testErrorMessageSubmitLanguageButton() {
+        String expectedResult = "Error: Precondition failed - Incomplete Input.";
+
+        getDriver().get("http://www.99-bottles-of-beer.net/submitnewlanguage.html ");
+        getDriver().findElement(
+                By.xpath("//input[@name='submitlanguage']")).click();
+        WebElement errorMessage = getDriver().findElement(
+                By.xpath("//div[@id='main']/p"));
+
+        Assert.assertEquals(errorMessage.getText(), expectedResult);
+    }
+}
