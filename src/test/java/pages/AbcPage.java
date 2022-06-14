22c63318543d601a970e2e599b7e195c83deaeeb
MaksPt <100244529+MaksPt@users.noreply.github.com>
2022-06-14 15:46:42 +0300

    AT_300.004 | created WebDriver, created Xpath CATEGORY_J, created getCategoryJ(), created getCategoryJClick(). (#27)

--- src/test/java/pages/AbcPage.java
+++ src/test/java/pages/AbcPage.java
@@ -1,4 +1,32 @@
 package pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
 public class AbcPage {
+
+    private WebDriver driver;
+
+    private final By CATEGORY_J = By.xpath("//a[@href='j.html']");
+
+    public AbcPage(WebDriver existingDriver) {
+
+        this.driver = existingDriver;
+    }
+
+    protected WebDriver getDriver() {
+
+        return driver;
+    }
+
+    public WebElement getCategoryJ() {
+
+        return getDriver().findElement(CATEGORY_J);
+    }
+
+    public void getCategoryJClick() {
+
+        getDriver().findElement(CATEGORY_J).click();
+    }
 }
