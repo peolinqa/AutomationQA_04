d1f1622adddb4eca90b0c5c528d54b9b8b45c07e
MaksPt <100244529+MaksPt@users.noreply.github.com>
2022-06-14 16:53:28 +0300

    AT_300.004 | JPage | created Xpath DESCRIPTION_J, created getDescriptionCategoryJ(), getDescriptionCategoryJText(). (#28)

--- src/test/java/pages/JPage.java
+++ src/test/java/pages/JPage.java
@@ -1,10 +1,14 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class JPage {
 
     private WebDriver driver;
+
+    private final By DESCRIPTION_J = By.xpath("//div[@id='main']/p[text()]");
 
     public JPage(WebDriver existingDriver) {
 
@@ -14,5 +18,15 @@ public class JPage {
     protected WebDriver getDriver() {
 
         return driver;
+    }
+
+    public WebElement getDescriptionCategoryJ() {
+
+        return getDriver().findElement(DESCRIPTION_J);
+    }
+
+    public String getDescriptionCategoryJText() {
+
+        return getDriver().findElement(DESCRIPTION_J).getText();
     }
 }
