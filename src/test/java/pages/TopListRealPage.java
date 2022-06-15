6df96c2d66c476ac4785cf27264e3c26dee58833
ServachakM <102581167+ServachakM@users.noreply.github.com>
2022-06-15 09:22:03 +0300

    RF_500_003 | Top Lists | Top Rated Real | Check table name + added methods and locator in TopListRealPage (#32)

--- src/test/java/pages/TopListRealPage.java
+++ src/test/java/pages/TopListRealPage.java
@@ -1,10 +1,14 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class TopListRealPage {
 
     private WebDriver driver;
+
+    private final By H2_TABLE_NAME = By.xpath("//div[@id='main']/h2");
 
     public TopListRealPage(WebDriver existingDriver) {
 
@@ -13,5 +17,15 @@ public class TopListRealPage {
     protected WebDriver getDriver() {
 
         return driver;
+    }
+
+    public WebElement getH2Table() {
+
+        return getDriver().findElement(H2_TABLE_NAME);
+    }
+
+    public String getH2TableName() {
+
+        return getH2Table().getText();
     }
 }
--- src/test/java/tests/CheckTableMenuTopListsTest.java
+++ src/test/java/tests/CheckTableMenuTopListsTest.java
@@ -3,22 +3,22 @@ package tests;
 import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import pages.TopListRealPage;
 import runner.BaseTest;
 
 public class CheckTableMenuTopListsTest extends BaseTest {
+    private static final String URL = "https://www.99-bottles-of-beer.net/toplist.html";
 
     @Test
     public void testTableNameMenuTopListsSubmenuTopRatedReal(){
-        final String URL = "https://www.99-bottles-of-beer.net/";
-
         String expectedResult = "Top Rated Real Languages";
 
         getDriver().get(URL);
-        getDriver().findElement(By.linkText("Top Lists")).click();
         getDriver().findElement(By.linkText("Top Rated Real")).click();
 
-        String actualResult = getDriver().findElement(
-                By.xpath("//div[@id='main']/h2")).getText();
+        TopListRealPage realPage = new TopListRealPage(getDriver());
+
+        String actualResult = realPage.getH2TableName().trim();
 
         Assert.assertEquals(actualResult,expectedResult);
     }
