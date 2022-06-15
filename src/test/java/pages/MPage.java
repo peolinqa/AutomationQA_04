220fd2b230f915bb960ea3cadcf2f7ef090935a9
MaksPt <100244529+MaksPt@users.noreply.github.com>
2022-06-15 21:05:25 +0300

    AT_300.007_AbcPage_MPage_CreateLocatorsAndMethods (#47)
    Added Locator LAST_LANGUAGE in MPage
    Added Methods getLastLanguageInM() and getLastLanguageInMText() in MPage

--- src/test/java/pages/MPage.java
+++ src/test/java/pages/MPage.java
@@ -1,8 +1,12 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class MPage {
+
+    private final By LAST_LANGUAGE = By.xpath("//table[@id='category']/tbody/tr[last()]/td/a");
 
     private WebDriver driver;
 
@@ -14,5 +18,15 @@ public class MPage {
     protected WebDriver getDriver() {
 
         return driver;
+    }
+
+    public WebElement getLastLanguageInM() {
+
+        return getDriver().findElement(LAST_LANGUAGE);
+    }
+
+    public String getLastLanguageInMText() {
+
+        return getLastLanguageInM().getText();
     }
 }
