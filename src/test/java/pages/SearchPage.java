b6b4ff076359cfaa98350d7d4224491cb06f740b
NadiaLidt <106011244+NadiaLidt@users.noreply.github.com>
2022-06-16 07:15:38 +0300

    AT_400.002 | [Search Languages] | Create locators and methods for the test Veryfi title Search Languages (#49)

--- src/test/java/pages/SearchPage.java
+++ src/test/java/pages/SearchPage.java
@@ -1,4 +1,55 @@
 package pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
 public class SearchPage {
+    private WebDriver driver;
+
+    private final By SEARCH_MENU = By.xpath("//div[@id='navigation']/ul[@id='menu']/li/a[@href='/search.html']");
+
+    private final By SEARCH_SUBMENU = By.xpath("//ul[@id='submenu']/li/a[@href='./search.html']");
+
+    private final By H2_MAIN =By.xpath("//div[@id='main']/h2");
+
+    public SearchPage(WebDriver existingDriver) {
+
+        this.driver = existingDriver;
+    }
+
+    protected WebDriver getDriver() {
+
+        return driver;
+    }
+
+    public WebElement getSearchMenu() {
+
+        return getDriver().findElement(SEARCH_MENU);
+    }
+
+    public void getSearchMenuClick() {
+
+        getSearchMenu().click();
+    }
+
+    public WebElement getSearchSubmenu() {
+
+        return getDriver().findElement(SEARCH_SUBMENU);
+    }
+
+    public void getSearchSubmenuClick() {
+
+        getSearchSubmenu().click();
+    }
+
+    public WebElement getH2Main() {
+
+        return getDriver().findElement(H2_MAIN);
+    }
+
+    public String getH2MainText() {
+
+        return getH2Main().getText();
+    }
 }
