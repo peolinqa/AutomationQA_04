c4b6b81c3900186093fbad8aa942942007ee49c1
Portnova Liudmila <103488693+08AlimDuiL@users.noreply.github.com>
2022-06-16 20:31:28 +0300

    Added СompareRealLinksSearchLanguagesTest (#63)

---
+++ src/test/java/tests/СompareRealLinksSearchLanguages.java
@@ -0,0 +1,31 @@
+package tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.MainPage;
+import runner.BaseTest;
+
+public class СompareRealLinksSearchLanguages extends BaseTest {
+
+    private static final String BASE_URL = "http://www.99-bottles-of-beer.net/";
+
+    @Test
+    public void testСompareSearchLanguages() {
+
+        String expectedResult = "http://www.99-bottles-of-beer.net/search.html";
+
+        getDriver().get(BASE_URL);
+
+        MainPage main = new MainPage(getDriver());
+
+        String actualResultHeader = main
+                .getSubmitSearchLanguagesMenu()
+                .getAttribute("href");
+        String actualResultFooter = main
+                .getFooterSearchLanguagesMenu()
+                .getAttribute("href");
+
+        Assert.assertEquals(actualResultHeader, expectedResult);
+        Assert.assertEquals(actualResultFooter, expectedResult);
+    }
+}
