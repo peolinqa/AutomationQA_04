b9eae1ed8aa8a691c2f3f1312b4dfeda2a7b225f
Morigor <103515819+Morigor@users.noreply.github.com>
2022-06-16 14:28:49 +0300

    AT_600.003 | Guestbook | Sign Guestbook | Error_Message (#60)

--- src/test/java/tests/MorigorProjectAllTest.java
+++ src/test/java/tests/MorigorProjectAllTest.java
@@ -3,11 +3,13 @@ package tests;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.MainPage;
+import pages.SignGuestbookPage;
 import runner.BaseTest;
 
 public class MorigorProjectAllTest extends BaseTest {
 
     private static final String BASE_URL = "http://www.99-bottles-of-beer.net/";
+    private static final String GUESTBOOK_URL = "https://www.99-bottles-of-beer.net/guestbookv2.html";
 
     @Test
     public void testVerifyTextOnMainPage() {
@@ -18,5 +20,19 @@ public class MorigorProjectAllTest extends BaseTest {
         MainPage main = new MainPage(getDriver());
 
         Assert.assertEquals(main.getH2HeaderText(), expectedResult);
+    }
+
+    @Test
+    public void testSignGuestbookErrorMessage() {
+        String expectedResult = "Error: Please enter at least a message, your email address and the security code.";
+
+        getDriver().get(GUESTBOOK_URL);
+
+        SignGuestbookPage signGuestbook = new SignGuestbookPage(getDriver());
+
+        signGuestbook.getSignGuestbookMenuClick();
+        signGuestbook.getSubmitButtonClick();
+
+        Assert.assertEquals(signGuestbook.getErrorMessageText(), expectedResult);
     }
 }
