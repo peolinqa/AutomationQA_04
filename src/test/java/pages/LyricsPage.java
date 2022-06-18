de42004c1b6f779ffe5f0122de2cf8c8e503d62d
ArinaJur <66705803+ArinaJur@users.noreply.github.com>
2022-06-18 13:04:48 -0400

    AT_200_011 [Start][Lyrics] Create locator for tags p, create method/methods to get a text from tags (#85)

--- src/test/java/pages/LyricsPage.java
+++ src/test/java/pages/LyricsPage.java
@@ -1,13 +1,82 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
 
 public class LyricsPage {
 
     private WebDriver driver;
 
+    private final By P_TAGS = By.xpath("//div[@id='main']/p");
+
     public LyricsPage(WebDriver existingDriver) {
 
         this.driver = existingDriver;
+    }
+
+    protected WebDriver getDriver() {
+
+        return driver;
+    }
+
+    private void getBottles(StringBuilder lyrics, int number, String btl) {
+        lyrics.append(number).append(btl);
+    }
+
+    private void getNoMore(StringBuilder lyrics, String noMore, String btl) {
+        lyrics.append(noMore).append(btl);
+    }
+
+    private String createLyrics() {
+        final String BOTTLES_WALL_CS = " bottles of beer on the wall, ";
+        final String BOTTLES_DOT_LN = " bottles of beer.\n";
+        final String BOTTLES_DOT = " bottles of beer on the wall.";
+        final String TAKE = "Take one down and pass it around, ";
+        final String GO = "Go to the store and buy some more, ";
+        final String NO_MORE = "No more";
+
+        StringBuilder lyrics = new StringBuilder();
+
+        getBottles(lyrics, 99, BOTTLES_WALL_CS);
+        getBottles(lyrics, 99, BOTTLES_DOT_LN);
+        for (int i = 98; i > -1; i--) {
+            lyrics.append(TAKE);
+
+            if (i == 1) {
+                getBottles(lyrics, i, BOTTLES_DOT.replace("s", ""));
+                getBottles(lyrics, i, BOTTLES_WALL_CS.replace("s", ""));
+                getBottles(lyrics, i, BOTTLES_DOT_LN.replace("s", ""));
+            } else if (i == 0) {
+                getNoMore(lyrics, NO_MORE.toLowerCase(), BOTTLES_DOT);
+                getNoMore(lyrics, NO_MORE, BOTTLES_WALL_CS);
+                getNoMore(lyrics, NO_MORE.toLowerCase(), BOTTLES_DOT_LN);
+            } else {
+                getBottles(lyrics, i, BOTTLES_DOT);
+                getBottles(lyrics, i, BOTTLES_WALL_CS);
+                getBottles(lyrics, i, BOTTLES_DOT_LN);
+            }
+        }
+        lyrics.append(GO);
+        getBottles(lyrics, 99, BOTTLES_DOT);
+
+        return lyrics.toString();
+    }
+
+    public List<WebElement> getPTags() {
+
+        return getDriver().findElements(P_TAGS);
+    }
+
+    public String getLyrics() {
+        StringBuilder result = new StringBuilder();
+
+        for (WebElement p : getPTags()) {
+            result.append(p.getText());
+        }
+
+        return result.toString();
     }
 }
