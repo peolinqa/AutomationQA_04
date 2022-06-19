8db8c346a3886ecd2793275e632b172f98cc812f
ktitova <ksunchik123@yandex.ru>
2022-06-19 20:41:44 +0300

    added class TopRatedRealPage

---
+++ src/test/java/pages/TopRatedRealPage.java
@@ -0,0 +1,34 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class TopRatedRealPage {
+
+    private WebDriver driver;
+
+    private final By TOP_RATED_REAL_MENU = By.xpath("//a[@href='./toplist_real.html']");
+
+    public TopRatedRealPage (WebDriver existingDriver) {
+
+        this.driver = existingDriver;
+    }
+
+    protected WebDriver getDriver() {
+
+        return driver;
+    }
+
+    public WebElement getTopRatedRealPage() {
+
+        return getDriver().findElement(TOP_RATED_REAL_MENU);
+    }
+
+    public void clickTopRatedRealPageClick() {
+
+        getTopRatedRealPage().click();
+    }
+
+
+}
