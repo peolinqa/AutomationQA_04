21677fee8fd0354081b4d9db1a8d57779042f53c
ServachakM <102581167+ServachakM@users.noreply.github.com>
2022-06-19 12:40:57 +0300

    RF 500.004/top list real page test table name menu top lists submenu top rated real to POM (#96)
    * RF_500.004 [TopListRealPage] testTableNameMenuTopListsSubmenuTopRatedReal to POM

--- src/test/java/tests/CheckTableMenuTopListsTest.java
+++ src/test/java/tests/CheckTableMenuTopListsTest.java
@@ -1,20 +1,28 @@
 package tests;
 
-import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import pages.MainPage;
+import pages.TopListPage;
 import pages.TopListRealPage;
 import runner.BaseTest;
 
 public class CheckTableMenuTopListsTest extends BaseTest {
-    private static final String URL = "https://www.99-bottles-of-beer.net/toplist.html";
+    private static final String URL = "https://www.99-bottles-of-beer.net/";
 
     @Test
     public void testTableNameMenuTopListsSubmenuTopRatedReal(){
         String expectedResult = "Top Rated Real Languages";
 
         getDriver().get(URL);
-        getDriver().findElement(By.linkText("Top Rated Real")).click();
+
+        MainPage main = new MainPage(getDriver());
+
+        main.clickTopListMenu();
+
+        TopListPage topList = new TopListPage(getDriver());
+
+        topList.clickMenuTopListReal();
 
         TopListRealPage realPage = new TopListRealPage(getDriver());
 
