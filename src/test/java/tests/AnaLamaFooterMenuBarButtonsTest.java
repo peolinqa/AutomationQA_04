691c2fa7ddb2783971bdf76632f9a5f0eaa9f430
AnaLama <104392942+AnaLama@users.noreply.github.com>
2022-06-20 12:02:51 -0400

    AT_100.004.002 | [MainPage] Footer | TestBrowseLanguageButton (#117)

--- src/test/java/tests/AnaLamaFooterMenuBarButtonsTest.java
+++ src/test/java/tests/AnaLamaFooterMenuBarButtonsTest.java
@@ -8,6 +8,7 @@ import runner.BaseTest;
 public class AnaLamaFooterMenuBarButtonsTest extends BaseTest {
 
     private static final String BASE_URL = "http://www.99-bottles-of-beer.net/";
+    private static final String BROWSE_LANGUAGE_PAGE_URL = "http://www.99-bottles-of-beer.net/abc.html";
 
     @Test
     public void testFooterMenuBarMainPage() {
@@ -21,5 +22,14 @@ public class AnaLamaFooterMenuBarButtonsTest extends BaseTest {
         String actualResult = main.getFooterMenuText();
 
         Assert.assertEquals(actualResult, expectedResult);
+    }
+    @Test
+    public void testFooterBrowseLanguageButton(){
+        getDriver().get(BASE_URL);
+        MainPage main = new MainPage(getDriver());
+
+        main.getBrowseLanguagesMenuClick();
+
+       Assert.assertEquals(getDriver().getCurrentUrl(), BROWSE_LANGUAGE_PAGE_URL);
     }
 }
