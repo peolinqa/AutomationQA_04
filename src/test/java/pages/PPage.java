e0eab6fbb48f3525929d0169308077f6f5a853f0
AnnaAbg <106620445+AnnaAbg@users.noreply.github.com>
2022-06-22 23:30:56 +0300

    AT_300_029 [AbcPage][P] Create locator and methods for Python Language (#152)

--- src/test/java/pages/PPage.java
+++ src/test/java/pages/PPage.java
@@ -1,10 +1,14 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class PPage {
 
     private final WebDriver driver;
+
+    private final By PYTHON_LANGUAGE = By.xpath("//a[@href='language-python-808.html']");
 
     public PPage(WebDriver existingDriver) {
 
@@ -14,5 +18,14 @@ public class PPage {
     protected WebDriver getDriver() {
 
         return driver;
+    }
+    public WebElement getPythonLanguageButton() {
+
+        return getDriver().findElement(PYTHON_LANGUAGE);
+    }
+
+    public void getPythonLanguageClick() {
+
+        getDriver().findElement(PYTHON_LANGUAGE).click();
     }
 }
