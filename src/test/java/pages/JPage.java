115757b84dd6a78c381077366d660f1989fa137e
IreneSnitko <103880495+IreneSnitko@users.noreply.github.com>
2022-06-22 10:02:18 +0300

    AT_300.018 [Browse Language] [J] Create the locator for tags a and method for get number programming languages (#136)

--- src/test/java/pages/JPage.java
+++ src/test/java/pages/JPage.java
@@ -4,12 +4,16 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class JPage {
 
     private WebDriver driver;
 
     private final By DESCRIPTION_J = By.xpath("//div[@id='main']/p[text()]");
     private final By H2_TITLE_J = By.xpath("//div[@id='main']/h2");
+    private final By A_LINKS = By.xpath("//tbody/tr/td/a[@href]");
 
     public JPage(WebDriver existingDriver) {
 
@@ -38,5 +42,20 @@ public class JPage {
     public String getH2PageJText() {
 
         return getH2PageJ().getText();
+    }
+
+    public int countProgrammingLanguages() {
+
+        List<WebElement> aHrefs
+                = getDriver()
+                .findElements(A_LINKS);
+
+        List<String> listOfLanguages = new ArrayList<>();
+
+        for (WebElement a : aHrefs) {
+            listOfLanguages.add(a.getText());
+        }
+
+        return listOfLanguages.size();
     }
 }
