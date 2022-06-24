78f86fba4a7799272c5c94c21c700f3d94768cbc
MaksPt <100244529+MaksPt@users.noreply.github.com>
2022-06-24 09:42:17 +0300

    AT_600.010 | [GuestbookV2Page][SignV2Page] | Verify error text (#177)

--- src/test/java/tests/MaksPtTest.java
+++ src/test/java/tests/MaksPtTest.java
@@ -2,10 +2,7 @@ package tests;
 
 import org.testng.Assert;
 import org.testng.annotations.Test;
-import pages.AbcPage;
-import pages.JPage;
-import pages.MPage;
-import pages.MainPage;
+import pages.*;
 import runner.BaseTest;
 
 public class MaksPtTest extends BaseTest {
@@ -46,5 +43,29 @@ public class MaksPtTest extends BaseTest {
         String actualResult = m.getLastLanguageInMText();
 
         Assert.assertEquals(actualResult, expectedResult);
+    }
+
+    @Test
+    public void testErrorTextSignV2Page() {
+        final String expectedTextError = "Error: Error: Invalid security code.";
+
+        getDriver().get(BASE_URL);
+
+        MainPage mainPage = new MainPage(getDriver());
+        mainPage.getGuestbookClick();
+
+        GuestbookV2Page guestbookV2Page = new GuestbookV2Page(getDriver());
+        guestbookV2Page.SignGuestbookClick();
+
+        SignV2Page signV2Page = new SignV2Page(getDriver());
+        signV2Page.inputNameField("MaksPt");
+        signV2Page.inputLocationField("Portugal");
+        signV2Page.inputEmailField("pt@gmail.com");
+        signV2Page.inputHomepageField("maks.com");
+        signV2Page.inputCaptchaField(Integer.toString((int) (Math.random() * 900 + 100)));
+        signV2Page.inputCommentField("QA Automation for everyone");
+        signV2Page.getSubmitButtonClick();
+
+        Assert.assertEquals(signV2Page.getErrorMessageText(), expectedTextError);
     }
 }
