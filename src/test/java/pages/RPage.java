3ea04987b477ff2f3b518af98aae94703fb93531
SvetlanaG1011 <103342736+SvetlanaG1011@users.noreply.github.com>
2022-06-24 00:40:39 -0600

    AT_300.035/RPage Locator TR_TAGS, method getTRTags, method countLanguages created (#176)

--- src/test/java/pages/RPage.java
+++ src/test/java/pages/RPage.java
@@ -1,10 +1,16 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
 
 public class RPage {
 
     private WebDriver driver;
+
+    private final By TR_TAGS = By.xpath("//tbody/tr[@onmouseover]");
 
     public RPage(WebDriver existingDriver) {
 
@@ -14,5 +20,15 @@ public class RPage {
     protected WebDriver getDriver() {
 
         return driver;
+    }
+
+    public List <WebElement> getTRTags() {
+
+        return getDriver().findElements(TR_TAGS);
+    }
+
+    public int countLanguages() {
+
+        return getTRTags().size();
     }
 }
