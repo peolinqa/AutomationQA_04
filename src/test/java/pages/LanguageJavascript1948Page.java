a7b5b88f860c536cfbe049b6128b89db6c6f564a
IreneSnitko <103880495+IreneSnitko@users.noreply.github.com>
2022-06-24 17:53:48 +0300

    AT_300.034 [Structure] [LanguageJavascript1948Page] Create LanguageJavascript1948Page and WebDriver (#168)

---
+++ src/test/java/pages/LanguageJavascript1948Page.java
@@ -0,0 +1,18 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class LanguageJavascript1948Page {
+
+    private WebDriver driver;
+
+    public LanguageJavascript1948Page(WebDriver existingDriver) {
+
+        this.driver = existingDriver;
+    }
+
+    protected WebDriver getDriver() {
+
+        return driver;
+    }
+}
