bfdb4520c595e5a1c990d9f10bb97148b0e5e2e0
MaksPt <100244529+MaksPt@users.noreply.github.com>
2022-06-24 08:58:55 +0300

    Created locators and methods in SignV2Page for the AT_600.010 | [GuestbookV2Page][SignV2Page] | Verify error text (#173)
    locator: NAME_FIELD;
             LOCATION_FIELD;
             EMAIL_FIELD;
             CAPTCHA_FIELD;
             COMMENT_FIELD;
    Methods: getNameField()
             inputNameField(String name)
             getLocationField()
             inputLocationField(String name)
             getEmailField()
             inputEmailField(String name)
             inputHomepageField(String name)
             getCaptchaField()
             inputCaptchaField(String name)
             getCommentField()
             inputCommentField(String name)

--- src/test/java/pages/SignV2Page.java
+++ src/test/java/pages/SignV2Page.java
@@ -13,6 +13,11 @@ public class SignV2Page {
     private final By SUBMIT = By.xpath("//input[@type='submit']");
     private final By ERROR_MESSAGE = By.xpath("//div[@id='main']/p");
     private final By HOMEPAGE_FIELD = By.xpath("//input[@name='homepage']");
+    private final By NAME_FIELD = By.xpath("//input[@name='name']");
+    private final By LOCATION_FIELD = By.xpath("//input[@name='location']");
+    private final By EMAIL_FIELD = By.xpath("//input[@name='email']");
+    private final By CAPTCHA_FIELD = By.xpath("//input[@name='captcha']");
+    private final By COMMENT_FIELD = By.xpath("//textarea[@name='comment']");
 
     public SignV2Page(WebDriver existingDriver) {
 
@@ -65,5 +70,54 @@ public class SignV2Page {
     public String getHomepageFieldAttribute() {
 
         return getHomepageField().getAttribute("value");
+    }
+
+    public WebElement getNameField() {
+
+        return getDriver().findElement(NAME_FIELD);
+    }
+
+    public void inputNameField(String name) {
+        getNameField().sendKeys(name);
+    }
+
+    public WebElement getLocationField() {
+
+        return getDriver().findElement(LOCATION_FIELD);
+    }
+
+    public void inputLocationField(String name) {
+        getLocationField().sendKeys(name);
+    }
+
+    public WebElement getEmailField() {
+
+        return getDriver().findElement(EMAIL_FIELD);
+    }
+
+    public void inputEmailField(String name) {
+        getEmailField().sendKeys(name);
+    }
+
+    public void inputHomepageField(String name) {
+        getHomepageField().sendKeys(name);
+    }
+
+    public WebElement getCaptchaField() {
+
+        return getDriver().findElement(CAPTCHA_FIELD);
+    }
+
+    public void inputCaptchaField(String name) {
+        getCaptchaField().sendKeys(name);
+    }
+
+    public WebElement getCommentField() {
+
+        return getDriver().findElement(COMMENT_FIELD);
+    }
+
+    public void inputCommentField(String name) {
+        getCommentField().sendKeys(name);
     }
 }
