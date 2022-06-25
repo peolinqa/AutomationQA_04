162b94ddecd032c942e5619a27e1bdae0636651f
KazakovaEV <104713123+KazakovaEV@users.noreply.github.com>
2022-06-25 18:55:10 +0300

    At 200.008/start text information about website wishes from team (#191)
    * AT_200.021 [Start] Text Information about the Website | Wishes from the Team | locator and methods created
    
    * AT_200.008 [Start] Text Information about the Website | Wishes from the Team | Test created

---
+++ src/test/java/tests/StartPageWishesTeamTest.java
@@ -0,0 +1,30 @@
+package tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.MainPage;
+import pages.StartPage;
+import runner.BaseTest;
+
+public class StartPageWishesTeamTest extends BaseTest {
+
+    private static final String BASE_URL = "http://www.99-bottles-of-beer.net/";
+
+    @Test
+    public void testWishesFromTeamStartPage() {
+
+        final String expectedResult = "Have a lot of fun,\n" +
+                "Oliver, Gregor and Stefan";
+
+        getDriver().get(BASE_URL);
+
+        MainPage startButton = new MainPage(getDriver());
+        startButton.getStartMenuClick();
+
+        StartPage start = new StartPage(getDriver());
+        String actualResult = start.getTeamWishesText();
+
+        Assert.assertEquals(actualResult, expectedResult);
+    }
+
+}
