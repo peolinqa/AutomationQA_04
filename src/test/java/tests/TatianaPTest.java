6f501bc9f6566638ac5016abb36e026f98107d05
Tatiana P <103320248+dmswlpk@users.noreply.github.com>
2022-06-25 07:18:06 -0600

    AT_400_001 | Search Languages | Search | Search java | (#112)

--- src/test/java/tests/TatianaPTest.java
+++ src/test/java/tests/TatianaPTest.java
@@ -1,8 +1,11 @@
 package tests;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import pages.MainPage;
+import pages.SearchPage;
 import pages.StartPage;
 import runner.BaseTest;
 
@@ -11,7 +14,7 @@ public class TatianaPTest extends BaseTest {
     private static final String BASE_URL = "https://www.99-bottles-of-beer.net/";
 
     @Test
-    public void testMainPageHeader(){
+    public void testMainPageHeader() {
         String expectedResult = "99 Bottles of Beer ".trim();
 
         getDriver().get(BASE_URL);
@@ -21,12 +24,34 @@ public class TatianaPTest extends BaseTest {
     }
 
     @Test
-    public void testStartPageContainsHistoryMenu(){
+    public void testStartPageContainsHistoryMenu() {
         String expectedResult = "History";
         getDriver().get(BASE_URL);
 
         StartPage start = new StartPage(getDriver());
 
-        Assert.assertEquals(start.getHistoryMenu().getText(),expectedResult);
+        Assert.assertEquals(start.getHistoryMenu().getText(), expectedResult);
+    }
+
+    @Test
+    public void testSearchFunction() {
+        String expectedResult = "Java (object-oriented version)";
+
+        getDriver().get(BASE_URL);
+
+        MainPage mainPage = new MainPage(getDriver());
+
+        mainPage.getSubmitSearchLanguagesMenu().click();
+
+        SearchPage searchPage = new SearchPage(getDriver());
+
+        searchPage.getSearchBoxSendKey("Java");
+        searchPage.getSubmitSearch().click();
+
+        WebElement findJava = getDriver().findElement(By.linkText("Java (object-oriented version)"));
+
+        String actualResult = findJava.getText();
+
+        Assert.assertEquals(actualResult, expectedResult);
     }
 }
