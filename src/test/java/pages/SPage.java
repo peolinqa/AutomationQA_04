ae2ff3fad3d8250ae6f8a72378bf0841b700f255
KazakovaEV <104713123+KazakovaEV@users.noreply.github.com>
2022-06-25 18:56:28 +0300

    AT_300.042 | [Browse Language] [SPage] | Create S Page | Structure | Locator of Shakespeare language | methods getShakespeareLanguage & getShakespeareLanguageClick (#199)

---
+++ src/test/java/pages/SPage.java
@@ -0,0 +1,29 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class SPage {
+
+    private WebDriver driver;
+
+    private final By SHAKESPEARE_LANGUAGE = By.xpath("//a[@href='language-shakespeare-664.html']");
+
+    public SPage(WebDriver existingDriver) {
+        this.driver = existingDriver;
+    }
+
+    protected WebDriver getDriver() {
+        return driver;
+    }
+
+    public WebElement getShakespeareLanguage() {
+        return getDriver().findElement(SHAKESPEARE_LANGUAGE);
+    }
+
+    public void getShakespeareLanguageClick() {
+        getShakespeareLanguage().click();
+    }
+
+}
