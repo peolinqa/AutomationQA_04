613e90aad67e8e179f06e3be88cbd428dcbee9a3
ViktoriiaKarpus <103855583+ViktoriiaKarpus@users.noreply.github.com>
2022-06-26 15:06:23 +0200

    ATAbcPageUPageCreateLocatorTABLEUAndMethodCountLanguagesUViktoriiaKarpus (#211)

--- src/test/java/pages/UPage.java
+++ src/test/java/pages/UPage.java
@@ -1,10 +1,16 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
 
 public class UPage {
 
     private WebDriver driver;
+
+    private final By TABLE_U = By.xpath("//tbody/tr[@onmouseover]");
 
     public UPage(WebDriver existingDriver) {
 
@@ -14,5 +20,15 @@ public class UPage {
     protected WebDriver getDriver() {
 
         return driver;
+    }
+
+    public List<WebElement> getTableU() {
+
+        return getDriver().findElements(TABLE_U);
+    }
+
+    public int countLanguagesU() {
+
+        return getTableU().size();
     }
 }
