fc0e1ef66464f30a0298573cbe81ec279e2a65d2
IreneSnitko <103880495+IreneSnitko@users.noreply.github.com>
2022-06-29 22:32:52 +0300

    [AT][LanguageJavascript1948Page] Create the locator and methods<IreneSnitko> (#247)

--- src/test/java/pages/LanguageJavascript1948Page.java
+++ src/test/java/pages/LanguageJavascript1948Page.java
@@ -1,10 +1,17 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class LanguageJavascript1948Page {
 
     private WebDriver driver;
+
+    private final By ALL_A_LINKS = By.xpath("//a[@href]");
 
     public LanguageJavascript1948Page(WebDriver existingDriver) {
 
@@ -14,5 +21,39 @@ public class LanguageJavascript1948Page {
     protected WebDriver getDriver() {
 
         return driver;
+    }
+
+    protected String getTextWikiLink() {
+
+        List<WebElement> aLinksOnJSPage
+                = getDriver()
+                .findElements(ALL_A_LINKS);
+        List<String> listAllALinks = new ArrayList<>();
+
+        for (WebElement a : aLinksOnJSPage) {
+            if (a.getText()
+                    .contains("http://en.wikipedia.org/wiki/Javascript")
+            ) {
+                listAllALinks.add(a.getText());
+            }
+        }
+
+        return listAllALinks
+                .toString()
+                .replace("[", "")
+                .replace("]", "");
+    }
+
+    protected WebElement getWikiLink() {
+
+        return getDriver().findElement(
+                By.linkText(getTextWikiLink()
+                )
+        );
+    }
+
+    public void clickWikiLink() {
+
+        getWikiLink().click();
     }
 }
