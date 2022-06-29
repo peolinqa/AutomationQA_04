d762457ec65c0ea8be023e0264e56049592457c2
Tatiana P <103320248+dmswlpk@users.noreply.github.com>
2022-06-29 07:33:49 -0600

    [AT][Main][navigation][menu][search] Search Java <dmswlpk> (#238)

--- src/test/java/tests/TatianaPTest.java
+++ src/test/java/tests/TatianaPTest.java
@@ -1,8 +1,11 @@
 package tests;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import pages.MainPage;
+import pages.SearchPage;
 import pages.StartPage;
 import runner.BaseTest;
 
@@ -31,5 +34,39 @@ public class TatianaPTest extends BaseTest {
         StartPage start = new StartPage(getDriver());
 
         Assert.assertEquals(start.getInfoSubmenu().getText(), expectedResult);
+    }
+
+    @Test
+    public void testSearchFunction() {
+        List<String> expectedResult = new ArrayList<>();
+        expectedResult.add("Java (object-oriented version)");
+        expectedResult.add("Java");
+        expectedResult.add("Java (exception oriented)");
+        expectedResult.add("Java (bytecode-version with loader)");
+        expectedResult.add("Java (Java 5.0 object-oriented version)");
+        expectedResult.add("Java (Singing with Java Speech API)");
+
+        getDriver().get(BASE_URL);
+
+        MainPage mainPage = new MainPage(getDriver());
+
+        mainPage.getSearchMenu().click();
+
+        SearchPage searchPage = new SearchPage(getDriver());
+
+        searchPage.getSearchBoxSendKey("Java");
+        searchPage.clickSubmitSearch();
+
+        List<WebElement> javaLnggs = getDriver().findElements(
+                By
+                        .xpath("//td/a[contains(@href, 'language')and (text()='Java' or contains(text(), 'Java ('))]"));
+
+        List<String> actualResult = new ArrayList<>();
+
+        for (WebElement name : javaLnggs) {
+            actualResult.add(name.getText());
+        }
+
+        Assert.assertEquals(actualResult, expectedResult);
     }
 }
