db60df7f0e5cdab5351fc022c5c3f5e2946ac17f
AnnaAbg <106620445+AnnaAbg@users.noreply.github.com>
2022-06-30 17:51:42 +0300

    [AT][Structure][LanguagePerl737Page] Create LanguagePerl737Page and Webdriver (#260)

---
+++ src/test/java/pages/LanguagePerl737Page.java
@@ -0,0 +1,18 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class LanguagePerl737Page {
+
+    private WebDriver driver;
+
+    public LanguagePerl737Page(WebDriver existingDriver) {
+
+        this.driver = existingDriver;
+    }
+
+    protected WebDriver getDriver() {
+
+        return driver;
+    }
+}
