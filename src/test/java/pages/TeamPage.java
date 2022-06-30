4accf80f67adb32bf1ebecb8d16d30ecaf9f1bef
Aleksandr Grazhdankin (aka Shurik) <99336539+ShurikGr@users.noreply.github.com>
2022-06-30 17:24:26 +0300

    Refactored testVerifyText and TeamPage class (#258)
    * Refactored testVerifyText and TeamPage class
    
    * [AT][Start][navigation][submenu][team] VerifyNavigation

--- src/test/java/pages/TeamPage.java
+++ src/test/java/pages/TeamPage.java
@@ -3,8 +3,6 @@ package pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
 
 import java.util.List;
 
@@ -14,20 +12,9 @@ public class TeamPage {
 
     private final By H3_TEAM_NAME = By.xpath("//div[@id = 'main']/h3");
 
-    @FindBy(xpath = "//div[@id='main']/p[2]")
-    private WebElement textAuthorOliverSchade;
-
-    @FindBy(xpath = "//div[@id='main']/p[4]")
-    private WebElement textAuthorGregorScheithauer;
-
-    @FindBy(xpath = "//div[@id='main']/p[6]")
-    private WebElement textAuthorStefanScheler;
-
-
     public TeamPage(WebDriver existingDriver) {
 
         this.driver = existingDriver;
-        PageFactory.initElements(getDriver(), this);
     }
 
     protected WebDriver getDriver() {
@@ -38,13 +25,5 @@ public class TeamPage {
     public List<WebElement> getListH3TeamName() {
 
         return getDriver().findElements(H3_TEAM_NAME);
-    }
-
-    public List<String> getTextFromAuthors() {
-
-        List<String> textFromAllAuthors =
-                List.of(textAuthorOliverSchade.getText(), textAuthorGregorScheithauer.getText(), textAuthorStefanScheler.getText());
-
-        return textFromAllAuthors;
     }
 }
