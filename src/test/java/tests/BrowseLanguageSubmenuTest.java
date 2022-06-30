f242d18ca7e78cb7c467b17da4d903466bb8d196
ServachakM <102581167+ServachakM@users.noreply.github.com>
2022-07-01 00:38:35 +0300

    [US][ABC][navigation][submenu][zero] Verify text, link text, navigation (#269)

--- src/test/java/tests/BrowseLanguageSubmenuTest.java
+++ src/test/java/tests/BrowseLanguageSubmenuTest.java
@@ -11,7 +11,7 @@ public class BrowseLanguageSubmenuTest extends BaseTest {
     private static final String BASE_URL = "https://www.99-bottles-of-beer.net/";
 
     @Test
-    public void testFirstSubmenuIs09() {
+    public void testVerifyTextZeroSubmenu() {
         String expectedResult = "0-9";
 
         getDriver().get(BASE_URL);
@@ -24,6 +24,43 @@ public class BrowseLanguageSubmenuTest extends BaseTest {
 
         String actualResult = abc.getZeroSubmenuText();
 
+        Assert.assertEquals(actualResult, expectedResult);
+    }
+
+    @Test
+    public void testVerifyLinkTextZeroSubmenu(){
+        String expectedResult = "https://www.99-bottles-of-beer.net/0.html";
+
+        getDriver().get(BASE_URL);
+
+        MainPage main = new MainPage(getDriver());
+
+        main.clickBrowseLanguagesMenu();
+
+        AbcPage abc = new AbcPage(getDriver());
+
+        String actualResult = abc.getZeroSubmenu().getAttribute("href");
+
+        Assert.assertEquals(actualResult, expectedResult);
+    }
+
+    @Test
+    public void testVerifyNavigationZeroSubmenu() {
+        String expectedResult = "99 Bottles of Beer | Browse category 0-9";
+
+        getDriver().get(BASE_URL);
+
+        MainPage main = new MainPage(getDriver());
+
+        main.clickBrowseLanguagesMenu();
+
+        AbcPage abc = new AbcPage(getDriver());
+
+        abc.getZeroSubmenu().click();
+
+        String actualResult = getDriver().getTitle();
+
+        Assert.assertTrue(abc.getZeroSubmenu().isDisplayed());
         Assert.assertEquals(actualResult, expectedResult);
     }
 
