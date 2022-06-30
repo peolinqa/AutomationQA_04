fc884a19c4942e43621dfb68e14948435f8bc4de
SvetlanaG1011 <103342736+SvetlanaG1011@users.noreply.github.com>
2022-06-30 01:13:00 -0600

    AT_StructureRAddLocatorsAndMethods (#251)

--- src/test/java/pages/RPage.java
+++ src/test/java/pages/RPage.java
@@ -11,6 +11,8 @@ public class RPage {
     private WebDriver driver;
 
     private final By TR_TAGS = By.xpath("//tbody/tr[@onmouseover]");
+    private final By H2_HEADER = By.xpath("//div[@id='main']/h2");
+    private final By P_TAG_TEXT = By.xpath("//div[@id='main']/p[1]");
 
     public RPage(WebDriver existingDriver) {
 
@@ -30,5 +32,30 @@ public class RPage {
     public int countLanguages() {
 
         return getTRTags().size();
+    }
+
+    public String getText(WebElement element) {
+
+        return element.getText();
+    }
+
+    public WebElement getH2Header() {
+
+        return getDriver().findElement(H2_HEADER);
+    }
+
+    public String getH2HeaderText() {
+
+        return getText(getH2Header());
+    }
+
+    public  WebElement getPTag() {
+
+        return getDriver().findElement(P_TAG_TEXT);
+    }
+
+    public String getPTagText() {
+
+        return getText(getPTag());
     }
 }
