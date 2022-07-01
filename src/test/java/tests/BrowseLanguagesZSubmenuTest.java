97ac625345ab511fc4b91158660aeca1dc1c3429
Liudmila Portnova <103488693+08AlimDuiL@users.noreply.github.com>
2022-07-01 20:02:45 +0300

    [AT][Z][main][text] Verify text (#278)
    * Added testZSubmenuDescription in BrowseLanguagesZSubmenuTest
    
    * Deleted two spaces

--- src/test/java/tests/BrowseLanguagesZSubmenuTest.java
+++ src/test/java/tests/BrowseLanguagesZSubmenuTest.java
@@ -12,6 +12,7 @@ public class BrowseLanguagesZSubmenuTest extends BaseTest {
     private static final String BASE_URL = "http://www.99-bottles-of-beer.net/";
 
     @Test
+
     public void testZSubmenuH2Header() {
 
         final String expectedResult = "Category Z";
@@ -26,6 +27,26 @@ public class BrowseLanguagesZSubmenuTest extends BaseTest {
 
         ZPage zSubmenu = new ZPage(getDriver());
         String actualResult = zSubmenu.getH2Text();
+
+        Assert.assertEquals(actualResult,expectedResult);
+    }
+
+    @Test
+    public void testZSubmenuDescription() {
+
+        final String expectedResult = "All languages starting with the letter" +
+                " Z are shown, sorted by Language.";
+
+        getDriver().get(BASE_URL);
+
+        MainPage main = new MainPage(getDriver());
+        main.clickBrowseLanguagesMenu();
+
+        AbcPage abc = new AbcPage(getDriver());
+        abc.clickZSubmenu();
+
+        ZPage zSubmenu = new ZPage(getDriver());
+        String actualResult = zSubmenu.getPTagsText();
 
         Assert.assertEquals(actualResult,expectedResult);
     }
