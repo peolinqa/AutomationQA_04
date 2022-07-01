1d96c0527fd648c83330f33cd473166137dc3093
Валентин Куприн (xbrookx) <72659405+ValentinKuprin@users.noreply.github.com>
2022-07-01 14:27:57 +0300

    [AT][Start][navigation][submenu][impressum] Verify Text (#273)

--- src/test/java/tests/Song99BoutleXbrookxTest.java
+++ src/test/java/tests/Song99BoutleXbrookxTest.java
@@ -3,10 +3,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-import pages.AbcPage;
-import pages.JPage;
-import pages.LanguageJava3Page;
-import pages.MainPage;
+import pages.*;
 import runner.BaseTest;
 import java.util.List;
 
@@ -16,21 +13,15 @@ public class Song99BoutleXbrookxTest extends BaseTest {
 
     @Test
     public void testConfirmTableWithHeaders() {
-        String expectedResult = "Language Author Date Comments Rate";
 
         getDriver().get(BASE_URL);
 
         MainPage main = new MainPage(getDriver());
         main.clickBrowseLanguagesMenu();
 
-        List<WebElement> list = getDriver().findElements(By.xpath("//div[@id='main']//th"));
+        AbcPage abc = new AbcPage(getDriver());
 
-        StringBuilder headers = new StringBuilder();
-        for (WebElement web : list) {
-            headers.append(web.getText()).append(" ");
-        }
-
-        Assert.assertEquals(headers.toString().trim(), expectedResult);
+        Assert.assertEquals(abc.getTitlesTable(), "Language, Author, Date, Comments, Rate");
     }
 
     @Test
@@ -55,4 +46,13 @@ public class Song99BoutleXbrookxTest extends BaseTest {
         Assert.assertEquals(javaVersions.size(), 6);
     }
 
+    @Test
+    public void testVerifyText () {
+
+        getDriver().get(BASE_URL);
+
+        StartPage startPage = new StartPage(getDriver());
+
+        Assert.assertEquals(startPage.getImpressumSubmenuText(), "Privacy");
+    }
 }
