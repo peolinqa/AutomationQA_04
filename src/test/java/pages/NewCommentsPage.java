7abea13536e8c403fe5a7779d15df939cad5fa34
Tatiana P <103320248+dmswlpk@users.noreply.github.com>
2022-06-30 23:43:56 -0600

    [AT][Structure][TopListPage][NewcommentsPage] Create Newcomments Page And Driver<dmswlpk> (#272)

---
+++ src/test/java/pages/NewCommentsPage.java
@@ -0,0 +1,21 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class NewCommentsPage {
+    private WebDriver driver;
+
+    private final By H2_MAIN = By.xpath("//div[@id='main']/h2");
+    private final By MAIN_P = By.xpath("h2[text()='New Comments']/following-sibling::p[1]");
+
+    public NewCommentsPage(WebDriver existingDriver){
+
+        this.driver = existingDriver;
+    }
+
+    protected WebDriver getDriver() {
+
+        return driver;
+    }
+}
