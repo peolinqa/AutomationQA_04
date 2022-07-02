00a4b48e65cd7f0cb625ea060ce3c4412239ef37
Tatiana P <103320248+dmswlpk@users.noreply.github.com>
2022-07-02 10:37:55 -0600

    ConflictResolving (#302)
    * ConflictResolving
    
    * Update ConflictResolvingPracticeTest.java
    
    * Update ConflictResolvingPracticeTest.java
    
    * Update ConflictResolvingPracticeTest.java

--- src/test/java/tests/ConflictResolvingPracticeTest.java
+++ src/test/java/tests/ConflictResolvingPracticeTest.java
@@ -7,6 +7,12 @@ import runner.BaseTest;
 
 public class ConflictResolvingPracticeTest extends BaseTest {
 
+public static final String BASE_URL = "http://www.99-bottles-of-beer.net/";
+
+   public static int CountTextP(int n) {
+
+        return n;
+    }
 
     public static String getText5(String text) {
 
@@ -14,7 +20,6 @@ public class ConflictResolvingPracticeTest extends BaseTest {
 
         return text.concat("Text2").concat("Text3");
 }
-    public static final String BASE_URL = "http://www.99-bottles-of-beer.net/";
 
     @Test
     public void testHistoricInformationLinkText() {
@@ -32,3 +37,4 @@ public class ConflictResolvingPracticeTest extends BaseTest {
         return text;
     }
 }
+
