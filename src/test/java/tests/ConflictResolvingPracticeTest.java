945767bce4aadbedaad4f130f4783b7102543001
Aleksandr Grazhdankin (aka Shurik) <99336539+ShurikGr@users.noreply.github.com>
2022-07-02 19:18:45 +0300

    new conflict (#304)
    * new conflict
    
    * Update ConflictResolvingPracticeTest.java

--- src/test/java/tests/ConflictResolvingPracticeTest.java
+++ src/test/java/tests/ConflictResolvingPracticeTest.java
@@ -1,8 +1,22 @@
 package tests;
 
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.StartPage;
 import runner.BaseTest;
 
 public class ConflictResolvingPracticeTest extends BaseTest {
+
+    public static final String BASE_URL = "http://www.99-bottles-of-beer.net/";
+
+    @Test
+    public void testHistoricInformationLinkText() {
+        getDriver().get(BASE_URL);
+
+        StartPage startPage = new StartPage(getDriver());
+
+        Assert.assertEquals(startPage.getHistoricInformationLinkText(), "historic information");
+    }
 
   public static String getTextljHEFJEJLFVJLWEVFWVFEF(String text, int number) {
 
@@ -10,7 +24,4 @@ public class ConflictResolvingPracticeTest extends BaseTest {
 
         return text;
     }
-
-
-
 }
