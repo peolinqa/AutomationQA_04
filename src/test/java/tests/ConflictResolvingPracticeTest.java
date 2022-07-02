894aaed897db9b78cddd63daccf1a36e15cbd48b
ArinaJur <66705803+ArinaJur@users.noreply.github.com>
2022-07-02 12:05:54 -0400

    Lesson conflict practice (#300)

--- src/test/java/tests/ConflictResolvingPracticeTest.java
+++ src/test/java/tests/ConflictResolvingPracticeTest.java
@@ -4,17 +4,12 @@ import runner.BaseTest;
 
 public class ConflictResolvingPracticeTest extends BaseTest {
 
+  public static String getTextljHEFJEJLFVJLWEVFWVFEF(String text, int number) {
 
-
-//    kjdgc
-
-
-
-    public static String getText(String text) {
+        number += number;
 
         return text;
     }
-
 
 
 
