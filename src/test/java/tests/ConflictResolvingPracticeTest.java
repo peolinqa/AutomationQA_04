a91ef4ce86cfce53561814eb685a9cb0c2bffe04
IreneSnitko <103880495+IreneSnitko@users.noreply.github.com>
2022-07-02 19:28:54 +0300

    Lesson conflict (#306)
    * Changed class/IreneSnitko
    
    * conflictResolving/IreneSnitko

--- src/test/java/tests/ConflictResolvingPracticeTest.java
+++ src/test/java/tests/ConflictResolvingPracticeTest.java
@@ -7,6 +7,13 @@ import runner.BaseTest;
 
 public class ConflictResolvingPracticeTest extends BaseTest {
 
+
+    public static String getText5(String text) {
+
+        String str = null;
+
+        return text.concat("Text2").concat("Text3");
+}
     public static final String BASE_URL = "http://www.99-bottles-of-beer.net/";
 
     @Test
