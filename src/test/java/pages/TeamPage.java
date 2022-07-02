b740dff74b50d69496f6526f1919ea63683aed7f
GalaKon <104635752+GalaKon@users.noreply.github.com>
2022-07-02 17:03:43 +0400

    [TC][Team][main][image]Verify images (#292)
    The locator and the method for testing number of team member's images are created

--- src/test/java/pages/TeamPage.java
+++ src/test/java/pages/TeamPage.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
+import java.sql.Array;
 import java.util.List;
 
 public class TeamPage {
@@ -11,6 +12,7 @@ public class TeamPage {
     private WebDriver driver;
 
     private final By H3_TEAM_NAME = By.xpath("//div[@id = 'main']/h3");
+    private final By GREGOR_SCHEITHAUER_STEFAN_SCHELER_IMAGES = By.xpath("//img[@src]");
 
     public TeamPage(WebDriver existingDriver) {
 
@@ -25,5 +27,20 @@ public class TeamPage {
     public List<WebElement> getListH3TeamName() {
 
         return getDriver().findElements(H3_TEAM_NAME);
+    }
+
+    public List<WebElement> getListTeamImages() {
+
+        return getDriver().findElements(GREGOR_SCHEITHAUER_STEFAN_SCHELER_IMAGES);
+    }
+
+    public int getAmmountOfImages() {
+
+        List<WebElement> Commentheaderslist;
+        Commentheaderslist = getDriver().findElements(GREGOR_SCHEITHAUER_STEFAN_SCHELER_IMAGES);
+
+        int count = Commentheaderslist.size();
+
+        return count;
     }
 }
