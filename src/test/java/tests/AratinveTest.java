eb966c0a8b35b19fb15dfc014f74dc4ddf1a6452
Aratinve <104650538+Aratinve@users.noreply.github.com>
2022-07-02 17:59:18 +0300

    AT_300.041/[Main][Abc]| Verify that the first submenu in Browse Language is 0-9 (#194)

---
+++ src/test/java/tests/AratinveTest.java
@@ -0,0 +1,29 @@
+package tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import runner.BaseTest;
+import pages.MainPage;
+import pages.AbcPage;
+
+public class AratinveTest extends BaseTest {
+
+    private static final String BASE_URL = "http://www.99-bottles-of-beer.net";
+
+    @Test
+    public void testSubmenuAbcTitle()  {
+        String expectedResult = "0-9";
+
+        getDriver().get(BASE_URL);
+
+        MainPage main = new MainPage(getDriver());
+
+        main.getBrowseLanguagesMenuClick();
+
+        AbcPage page = new AbcPage(getDriver());
+
+        String actualResult = page.getCategory09Text();
+
+        Assert.assertEquals(actualResult, expectedResult);
+    }
+}
