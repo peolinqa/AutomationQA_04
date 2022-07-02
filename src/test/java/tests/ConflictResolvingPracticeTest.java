574360ffe5db38ee0905c191dd40eea0227e1036
PocheKirya <104856973+PocheKirya@users.noreply.github.com>
2022-07-02 19:45:20 +0300

    Create ConflictResolvingPracticeTest.java

--- src/test/java/tests/ConflictResolvingPracticeTest.java
+++ src/test/java/tests/ConflictResolvingPracticeTest.java
@@ -8,7 +8,7 @@ import pages.StartPage;
 import runner.BaseTest;
 
 public class ConflictResolvingPracticeTest extends BaseTest {
-  }
+ 
 
 public static String getText(String text) {
     
@@ -34,17 +34,6 @@ public static String getText5(String text) {
    }
 
  public static final String BASE_URL = "http://www.99-bottles-of-beer.net/";
-        
-   return text.concat("Text2").concat("Text3");
-   }
-
-    public void testHistoricInformationLinkText2() {
-        getDriver().get(BASE_URL);
-
-        StartPage startPage = new StartPage(getDriver());
-
-        Assert.assertEquals(startPage.getHistoricInformationLinkText(), "historic information");
-    }
 
     @Test
     public void testHistoricInformationLinkText() {
@@ -54,26 +43,6 @@ public static String getText5(String text) {
 
         Assert.assertEquals(startPage.getHistoricInformationLinkText(), "historic information");
     }
-
-
-        public static String getText(String text) {
-
-        return text;
-    }
-
-    @Test
-    public void testCheckSubmenuPrivacyIsClickable(){
-        String expectedResult = "Privacy";
-
-        getDriver().get(BASE_URL);
-
-        StartPage startPage = new StartPage(getDriver());
-
-        String actualResult = startPage.getImpressumSubmenuText();
-
-        Assert.assertEquals(actualResult,expectedResult);
-    }
-
   public static String getTextljHEFJEJLFVJLWEVFWVFEF(String text, int number) {
 
     number += number;
