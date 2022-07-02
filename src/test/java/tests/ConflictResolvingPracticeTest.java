6c9a7f797d89ddbe0f6fa0cfe043a255c9e6af24
Irina Sh <104162505+Kononirin@users.noreply.github.com>
2022-07-02 22:05:21 +0500

    conflictTry (#305)
    * conflictTry
    
    * Update ConflictResolvingPracticeTest.java
    
    * Update ConflictResolvingPracticeTest.java

--- src/test/java/tests/ConflictResolvingPracticeTest.java
+++ src/test/java/tests/ConflictResolvingPracticeTest.java
@@ -11,4 +11,42 @@ import runner.BaseTest;
 
 public class ConflictResolvingPracticeTest extends BaseTest {
 
+
+  public static String getTextljHEFJEJLFVJLWEVFWVFEF(String text, int number) {
+  
+  number += number;
+  
+  return text;  
+ }
+
+public static final String BASE_URL = "http://www.99-bottles-of-beer.net/";
+
+   public static int CountTextP(int n) {
+
+        return n;
+    }
+
+
+    public static String getTextFourFive(String text) {
+
+        return text;
+    }
+
+    private final By H2_TITLE_A = By.xpath("//div[@id='main']/h2");
+
+    public WebElement getH2PageA() {
+
+        return getDriver().findElement(H2_TITLE_A);
+    }
+
+  public static String getText(String text) {
+   return text;
+  }
+
+    public static String getText5(String text) {
+
+        String str = null;
+
+        return text.concat("Text2").concat("Text3");
+  }
 }
