22daf5b7647d5c4125b85dc03aa03ae50a6dde7c
AnnaAbg <106620445+AnnaAbg@users.noreply.github.com>
2022-07-08 20:32:39 +0300

    [AT][[Structure][LanguageCSharp1614Page] Create LanguageCSharp1614Page and Webdriver <AnnaAbg> (#401)

---
+++ src/test/java/pages/browse_languages/languages/LanguageCSharp1614Page.java
@@ -0,0 +1,18 @@
+package pages.browse_languages.languages;
+
+import org.openqa.selenium.WebDriver;
+
+public class LanguageCSharp1614Page {
+
+    private final WebDriver driver;
+
+    public LanguageCSharp1614Page(WebDriver existingDriver) {
+
+        this.driver = existingDriver;
+    }
+
+    protected WebDriver getDriver() {
+
+        return driver;
+    }
+}
