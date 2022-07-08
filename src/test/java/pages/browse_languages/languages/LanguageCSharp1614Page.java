bcc760b6748e835b31468a88cd46c38097e3a249
AnnaAbg <106620445+AnnaAbg@users.noreply.github.com>
2022-07-08 22:11:22 +0300

    [AT][Structure][LanguageCSharp1614Page] Create locator and methods <AnnaAbg> (#406)

--- src/test/java/pages/browse_languages/languages/LanguageCSharp1614Page.java
+++ src/test/java/pages/browse_languages/languages/LanguageCSharp1614Page.java
@@ -1,8 +1,15 @@
 package pages.browse_languages.languages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class LanguageCSharp1614Page {
+
+    private final By CSharp_Versions = By.xpath("//table[@id='category']/tbody/tr/td/a[@href]");
 
     private final WebDriver driver;
 
@@ -14,5 +21,22 @@ public class LanguageCSharp1614Page {
     protected WebDriver getDriver() {
 
         return driver;
+    }
+
+    public List<String> getCSharpLanguageALinks() {
+
+        List<WebElement> aHrefs = getDriver().findElements(CSharp_Versions);
+        List<String> listOfVersions = new ArrayList<>();
+
+        for (WebElement tr : aHrefs ) {
+            listOfVersions.add(tr.getText());
+        }
+
+        return listOfVersions;
+    }
+
+    public int getCSharpLanguageNumberOfVersions() {
+
+        return getCSharpLanguageALinks().size();
     }
 }
