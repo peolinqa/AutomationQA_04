095b0532e48e19c8cd8e30efb624c90bf586f045
ktitova <ksunchik123@yandex.ru>
2022-07-13 17:20:37 +0300

    added class ToplistAssemblyTest

---
+++ src/test/java/tests/top_lists/ToplistAssemblyTest.java
@@ -0,0 +1,21 @@
+package tests.top_lists;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import runner.BaseTest;
+
+public class ToplistAssemblyTest extends BaseTest {
+
+    @Test
+    public void testGoToTheTopRatedAssemblyLanguages() {
+        String expectedResult = "Top Rated Assembly Languages";
+
+        String actualResult =
+                openBaseURL()
+                        .clickTopListMenu()
+                        .clickTopListAssemblySubmenu()
+                        .getH2TableAssemblyName();
+
+        Assert.assertEquals(actualResult, expectedResult);
+    }
+}
