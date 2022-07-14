226796dd3f693514503fe193ea34e6833670876b
SvetlanaG1011 <103342736+SvetlanaG1011@users.noreply.github.com>
2022-07-14 00:37:20 -0500

    Class SvetlanaGTest deleted, tests refactored and moved to RTest (#518)

--- src/test/java/tests/browse_languages/letters/RTest.java
+++ src/test/java/tests/browse_languages/letters/RTest.java
@@ -3,6 +3,8 @@ package tests.browse_languages.letters;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import pages.MainPage;
+import pages.browse_languages.AbcPage;
 import pages.browse_languages.letters.RPage;
 import runner.BaseTest;
 
@@ -11,8 +13,8 @@ import java.util.List;
 
 public class RTest extends BaseTest {
 
-    @Test (description = "[US][Browse Language][R Page]")
-    public void testInformationAboutLanguageRPGIVFreeForm(){
+    @Test(description = "[US][Browse Language][R Page]")
+    public void testInformationAboutLanguageRPGIVFreeForm() {
         String languageExpected = "RPG IV Free-Form";
         String authorExpected = "James Wall";
         String dateExpected = "01/10/06";
@@ -41,6 +43,65 @@ public class RTest extends BaseTest {
         Assert.assertEquals(actualInformationAboutLanguageRPGIVFreeForm.size(), 1);
         Assert.assertTrue(!actualInformationAboutLanguageRPGIVFreeForm.get(0).isEmpty());
         Assert.assertEquals(actualInformationAboutLanguageRPGIVFreeForm.get(0)
-                ,expectedInformationAboutLanguageRPGIVFreeForm.toString());
+                , expectedInformationAboutLanguageRPGIVFreeForm.toString());
+    }
+
+    @Test
+    public void testHowManyLanguagesOnPageR() {
+        final int expectedQuantity = 44;
+
+        int actualQuantity =
+                openBaseURL()
+                        .clickBrowseLanguagesMenu()
+                        .clickRSubmenu()
+                        .getCountTrTags();
+
+        Assert.assertEquals(actualQuantity, expectedQuantity);
+    }
+
+    @Test
+    public void testH2HeaderOnPageR() {
+        final String expectedHeader = "Category R";
+
+        String actualHeader =
+                openBaseURL()
+                        .clickBrowseLanguagesMenu()
+                        .clickRSubmenu()
+                        .getH2HeaderText();
+
+        Assert.assertEquals(actualHeader, expectedHeader);
+    }
+
+    @Test
+    public void testPTagTextOnPageR() {
+        final String expectedText = "All languages starting with the letter R are shown, sorted by Language.";
+
+        String actualText =
+                openBaseURL()
+                        .clickBrowseLanguagesMenu()
+                        .clickRSubmenu()
+                        .getPTagText();
+
+        Assert.assertEquals(actualText, expectedText);
+    }
+
+    @Test
+    public void testTableHeaderTextOnPageR() {
+
+        final List<String> expectedTableHeader = new ArrayList<>();
+
+        expectedTableHeader.add("Language");
+        expectedTableHeader.add("Author");
+        expectedTableHeader.add("Date");
+        expectedTableHeader.add("Comments");
+        expectedTableHeader.add("Rate");
+
+        List<String> actualTableHeader =
+                openBaseURL()
+                        .clickBrowseLanguagesMenu()
+                        .clickRSubmenu()
+                        .getTextListThTags();
+
+        Assert.assertEquals(actualTableHeader, expectedTableHeader);
     }
 }
