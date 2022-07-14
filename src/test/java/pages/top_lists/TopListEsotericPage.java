7b02b8165c487b82e338c815e7e17dd4d1f97819
ktitova <ksunchik123@yandex.ru>
2022-07-14 16:28:32 +0300

    refactor ToplistEsotericTest

--- src/test/java/pages/top_lists/TopListEsotericPage.java
+++ src/test/java/pages/top_lists/TopListEsotericPage.java
@@ -1,26 +1,11 @@
 package pages.top_lists;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import pages.BasePage;
 
-public class TopListEsotericPage extends BasePage {
-
-    private final By H2_TABLE_NAME_ESOTERIC = By.xpath("//div[@id='main']/h2");
+public class TopListEsotericPage extends _TopPage {
 
     public TopListEsotericPage(WebDriver driver) {
 
        super(driver);
-    }
-
-    public WebElement getH2TableEsoteric() {
-
-        return getDriver().findElement(H2_TABLE_NAME_ESOTERIC);
-    }
-
-    public String getH2TableEsotericName() {
-
-        return getH2TableEsoteric().getText();
     }
 }
