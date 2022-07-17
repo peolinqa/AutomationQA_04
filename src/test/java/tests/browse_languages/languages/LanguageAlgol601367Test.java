ce564884af6bfe6fdf1d5dfca43b32cd4b81734f
SafronovaOlga <104982522+SafronovaOlga@users.noreply.github.com>
2022-07-17 14:09:36 +0200

    AT/Created LanguageAlgol601367Test and deleted test in ATest (#555)

---
+++ src/test/java/tests/browse_languages/languages/LanguageAlgol601367Test.java
@@ -0,0 +1,21 @@
+package tests.browse_languages.languages;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.browse_languages.languages.LanguagesPage;
+import runner.BaseTest;
+
+public class LanguageAlgol601367Test extends BaseTest {
+    @Test
+    public void testLanguageAlgol601367() {
+
+                openBaseURL()
+                .clickBrowseLanguagesMenu().clickASubmenu().clickLanguage("Algol60");
+
+
+        LanguagesPage al601367 = new LanguagesPage(getDriver());
+        String algol601367Title = al601367.getH2VotingText();
+
+        Assert.assertNotEquals(algol601367Title, getExternalPageURL());
+    }
+}
--- src/test/java/tests/browse_languages/letters/ATest.java
+++ src/test/java/tests/browse_languages/letters/ATest.java
@@ -5,8 +5,6 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import runner.BaseTest;
 
-import static runner.TestUtils.getTrWithRequiredLanguage;
-
 public class ATest extends BaseTest {
 
     @Test
@@ -19,19 +17,5 @@ public class ATest extends BaseTest {
                         .getASubmenuText();
 
         Assert.assertEquals(actualASubmenuTitle, expectedASubmenuTitle);
-    }
-
-    @Test
-    public void testVerifyLanguageAlgol60() {
-        String languageName = "Algol60";
-
-        String expectedResult = getTrWithRequiredLanguage(languageName);
-
-        String actualResult = openBaseURL()
-                .clickBrowseLanguagesMenu()
-                .clickJSubmenu()
-                .getTrText(languageName);
-
-        Assert.assertEquals(actualResult, expectedResult);
     }
 }
