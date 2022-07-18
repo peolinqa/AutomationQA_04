0427642cbc0745124601416b2e160dbf62fa4de6
sabinasaad <104028290+sabinasaad@users.noreply.github.com>
2022-07-18 20:15:41 +0300

    [RF][LyricsTest]refactor FirstTest and move to LyricsTest<sabinasaad> (#576)

--- src/test/java/tests/start/LyricsTest.java
+++ src/test/java/tests/start/LyricsTest.java
@@ -9,6 +9,19 @@ import static runner.TestUtils.createSongLyricsTextUsingAlgorithm;
 public class LyricsTest extends BaseTest {
 
     @Test
+    public void testH2MainText() {
+        final String expectedH2MainText = "Lyrics of the song 99 Bottles of Beer";
+
+        String actualH2MainText =
+                openBaseURL()
+                        .clickStartMenu()
+                        .clickSongLyricsSubmenu()
+                        .getH2MainText();
+
+        Assert.assertEquals(actualH2MainText, expectedH2MainText);
+    }
+
+    @Test
     public void testSongLyricsText() {
         String expectedSongLyricsText = createSongLyricsTextUsingAlgorithm();
 
